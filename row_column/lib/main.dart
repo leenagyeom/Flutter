c30acae3975beb548806b1e44ef1340c6733404f
leenagyeom <leenagyeom91@gmail.com>
2022-06-06 00:59:00 +0900

    Container 배치하기 실습

---
+++ row_column/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:row_column/Screen/home_screen.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
