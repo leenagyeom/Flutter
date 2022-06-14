419b60adbe36fbfb1712a570353cf3093d0856a0
leenagyeom <leenagyeom91@gmail.com>
2022-06-15 02:12:42 +0900

    statefulWidget 생명주기 확인

--- StatefulWidget/lib/screen/home_screen.dart
+++ StatefulWidget/lib/screen/home_screen.dart
@@ -2,22 +2,74 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   final Color color;
-  const HomeScreen({
+
+  HomeScreen({
     required this.color,
     Key? key,
-  }) : super(key: key);
+  }) : super(key: key) {
+    print('Widget Constructor 실행');
+  }
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() {
+    print('createState 실행!');
+    return _HomeScreenState();
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int number = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState 실행!');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('didChangeDependencies 실행!');
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate 실행!');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('dispose 실행!');
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print('didUpdateWidget 실행!');
+    super.didUpdateWidget(oldWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50.0,
-      height: 50.0,
-      color: widget.color,
+    print('build 실행');
+    return GestureDetector(
+      onDoubleTap: (){
+        setState(() {
+          number++;
+        });
+        print('클릭!');
+      },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color,
+        child: Center(
+          child: Text(number.toString(),
+          ),
+        ),
+      ),
     );
   }
 }
